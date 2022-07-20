4dea0f19afa4dbf7a07ccd96473cc3a69d74b42f
khaledyd <77590422+khaledyd@users.noreply.github.com>
2022-07-20 18:13:31 +0300

    Add files via upload

---
+++ src/components/scifi/Scifi.jsx
@@ -0,0 +1,11 @@
+import "./scifi.css";
+
+export default function Scifi(props) {
+  return (
+    <div className="scifi">
+    <div className="scifii">
+        <img src= {props.img} alt = {props.alt}/>
+    </div>
+    </div>
+  );
+}
