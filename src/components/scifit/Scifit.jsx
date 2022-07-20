9ca1bf9b5b6095c8fb7dcaa756c7d5340e09c47a
khaledyd <77590422+khaledyd@users.noreply.github.com>
2022-07-20 18:14:37 +0300

    Add files via upload

---
+++ src/components/scifit/Scifit.jsx
@@ -0,0 +1,12 @@
+import "./scifit.css";
+
+export default function Scifit() {
+  return (
+    <div className="scifit">
+      <div className="scifiti">
+        <p className="next-text">Sci-fi shows</p>
+        <i class="fa-solid fa-angle-right"></i>
+      </div>
+    </div>
+  );
+}
