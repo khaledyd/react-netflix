104ad10b7218505d913b026c3ea96c828995d1d1
khaledyd <77590422+khaledyd@users.noreply.github.com>
2022-07-20 14:24:50 +0300

    Add files via upload

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <h1>hi</h1>
+      <div className="footer2">
+        <p></p>
+      </div>
+    </div>
+    
+  );
+}
