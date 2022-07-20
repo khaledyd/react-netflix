961972b31bee19ecc65dc8cb3abf4f393722cfe1
khaledyd <77590422+khaledyd@users.noreply.github.com>
2022-07-20 18:17:32 +0300

    Add files via upload

---
+++ src/components/topten/Topten.jsx
@@ -0,0 +1,12 @@
+import "./topten.css";
+
+export default function Topten() {
+  return (
+    <div className="topten">
+      <div className="top">
+        <p className="next-text">Top 10 in your country</p>
+        <i class="fa-solid fa-angle-right"></i>
+      </div>
+    </div>
+  );
+}
