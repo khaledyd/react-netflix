d0ef6835e6c82acde56e06f5245358872a302b7d
khaledyd <77590422+khaledyd@users.noreply.github.com>
2022-07-20 18:19:34 +0300

    Add files via upload

---
+++ src/components/trending/Trending.jsx
@@ -0,0 +1,12 @@
+import "./trending.css";
+
+export default function Trending() {
+  return (
+    <div className="trending">
+      <div className="trendingt">
+        <p className="next-text">Trending now</p>
+        <i class="fa-solid fa-angle-right"></i>
+      </div>
+    </div>
+  );
+}
