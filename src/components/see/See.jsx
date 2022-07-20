28c50d2654f3ce9ba3f0f9784caad459896fcebb
khaledyd <77590422+khaledyd@users.noreply.github.com>
2022-07-20 18:15:46 +0300

    Add files via upload

---
+++ src/components/see/See.jsx
@@ -0,0 +1,12 @@
+import "./see.css";
+
+export default function See() {
+  return (
+    <div className="see">
+     <div className="next">
+         <p className="next-text">see again</p>
+         <i class="fa-solid fa-angle-right"></i>
+     </div>
+    </div>
+  );
+}
