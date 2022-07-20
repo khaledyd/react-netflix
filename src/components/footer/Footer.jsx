9e218a34037a5e108ad5642cbff8ad13641975f8
khaledyd <77590422+khaledyd@users.noreply.github.com>
2022-07-20 14:27:00 +0300

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,6 @@ import "./footer.css";
 export default function Footer() {
   return (
     <div className="footer">
-      <h1>hi</h1>
       <div className="footer2">
         <p></p>
       </div>
